5b72bd1020badd03f760a0481c4387fba9f6850a
Benjamin Wasty <benny.wasty@gmail.com>
2018-12-28 21:08:22 +0100

    draft some utils

---
+++ vulkan-utils.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+namespace vkutils {
+
+vk::UniqueInstance createInstance(
+    const char* applicationName = "Application",
+    bool enableValidationLayers = false, 
+    const std::vector<const char*>& validationLayers = { "VK_LAYER_LUNARG_standard_validation" }
+) {
+    if (enableValidationLayers && !checkValidationLayerSupport(validationLayers)) {
+        throw std::runtime_error("validation layers requested, but not available!");
+    }
+
+    auto appInfo = vk::ApplicationInfo(
+        applicationName,
+        VK_MAKE_VERSION(1, 0, 0),
+        "No Engine",
+        VK_MAKE_VERSION(1, 0, 0),
+        VK_API_VERSION_1_0
+    );
+
+    auto extensions = getRequiredExtensions(enableValidationLayers);
+
+    auto createInfo = vk::InstanceCreateInfo(
+        vk::InstanceCreateFlags(),
+        &appInfo,
+        0, nullptr, // enabled layers
+        static_cast<uint32_t>(extensions.size()), extensions.data() // enabled extensions
+    );
+
+    if (enableValidationLayers) {
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+    }
+
+    try {
+        return vk::createInstanceUnique(createInfo, nullptr);
+    }
+    catch (vk::SystemError err) {
+        throw std::runtime_error("failed to create instance!");
+    }
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+    return VK_FALSE;
+}
+
+VkDebugUtilsMessengerEXT setupDebugCallback(const vk::Instance& instance) {
+    auto createInfo = vk::DebugUtilsMessengerCreateInfoEXT(
+        vk::DebugUtilsMessengerCreateFlagsEXT(),
+        vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose | vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning | vk::DebugUtilsMessageSeverityFlagBitsEXT::eError,
+        vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance,
+        debugCallback,
+        nullptr
+    );
+
+    // NOTE: Vulkan-hpp has methods for this, but they trigger linking errors...
+    //instance->createDebugUtilsMessengerEXT(createInfo);
+    //instance->createDebugUtilsMessengerEXTUnique(createInfo);
+
+    VkDebugUtilsMessengerEXT callback;
+    // NOTE: reinterpret_cast is also used by vulkan.hpp internally for all these structs
+    if (CreateDebugUtilsMessengerEXT(instance, reinterpret_cast<const VkDebugUtilsMessengerCreateInfoEXT*>(&createInfo), nullptr, &callback) != VK_SUCCESS) {
+        throw std::runtime_error("failed to set up debug callback!");
+    }
+
+    return callback;
+}
+
+vk::SurfaceKHR createSurface(const vk::Instance& instance, GLFWwindow* window) {
+    VkSurfaceKHR rawSurface;
+    if (glfwCreateWindowSurface(instance, window, nullptr, &rawSurface) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create window surface!");
+    }
+
+    vk::SurfaceKHR surface = rawSurface;
+    return surface;
+}
+
+////////
+
+std::vector<const char*> getRequiredExtensions(bool enableValidationLayers) {
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+    if (enableValidationLayers) {
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    return extensions;
+}
+
+bool checkValidationLayerSupport(const std::vector<const char*>& validationLayers) {
+    auto availableLayers = vk::enumerateInstanceLayerProperties();
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pCallback) {
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        return func(instance, pCreateInfo, pAllocator, pCallback);
+    }
+    else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT callback, const VkAllocationCallbacks* pAllocator) {
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        func(instance, callback, pAllocator);
+    }
+}
+
+} // namespace
